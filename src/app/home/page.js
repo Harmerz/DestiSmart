09e7e393e0b3c33b8352a396a9f83bf76f1a68b2
Haikal Hilmi <haikal6255hilmi@gmail.com>
2024-05-26 01:39:00 +0700

    feat(home): with bottom bar

---
+++ src/app/home/page.js
@@ -0,0 +1,9 @@
+import { BottomBar } from '@/components/bottomBar'
+
+export default function HomePage() {
+  return (
+    <div className="w-full bg-background">
+      <BottomBar />
+    </div>
+  )
+}
---
+++ src/components/bottomBar.js
@@ -0,0 +1,33 @@
+import { IoMdHome } from 'react-icons/io'
+import { FaMapLocation, FaCalendarCheck } from 'react-icons/fa6'
+import { IoChatbubbleEllipses, IoHomeSharp } from 'react-icons/io5'
+import Link from 'next/link'
+
+export function BottomBar() {
+  return (
+    <div className="absolute bottom-0 grid w-full max-w-[414px] grid-cols-4 justify-center">
+      <Link href="/home">
+        <div className="flex w-full justify-center py-3">
+          <IoHomeSharp className="h-6 w-6" />
+        </div>
+      </Link>
+      <Link href="/map">
+        <div className="flex w-full justify-center py-3">
+          <FaMapLocation className="h-6 w-6" />
+        </div>
+      </Link>
+      <Link href="/plan">
+        <div className="flex w-full justify-center py-3">
+          <FaCalendarCheck className="h-6 w-6" />
+        </div>
+      </Link>
+      <Link href="/chat">
+        <div className="flex w-full justify-center py-3">
+          <IoChatbubbleEllipses className="h-6 w-6" />
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default BottomBar
