fc91637eb243f4ef41fe6b2a30555c82e63c0624
Haikal Hilmi <haikal6255hilmi@gmail.com>
2024-06-01 23:42:05 +0700

    feat(bottombar): responsive bar

--- src/components/bottomBar.js
+++ src/components/bottomBar.js
@@ -5,25 +5,29 @@ import Link from 'next/link'
 
 export function BottomBar() {
   return (
-    <div className="z-20 grid w-full max-w-[414px] grid-cols-4 justify-center border-t bg-background">
+    <div className="z-20 grid w-full grid-cols-4 justify-center border-t bg-background">
       <Link href="/home">
-        <div className="flex w-full justify-center py-3">
+        <div className="flex w-full items-center justify-center py-3 sm:gap-2">
           <IoHomeSharp className="h-6 w-6" />
+          <p className="hidden sm:block">Home</p>
         </div>
       </Link>
       <Link href="/map">
-        <div className="flex w-full justify-center py-3">
+        <div className="flex w-full items-center justify-center py-3 sm:gap-2">
           <FaMapLocation className="h-6 w-6" />
+          <p className="hidden sm:block">Location</p>
         </div>
       </Link>
       <Link href="/plan">
-        <div className="flex w-full justify-center py-3">
+        <div className="flex w-full items-center justify-center py-3 sm:gap-2">
           <FaCalendarCheck className="h-6 w-6" />
+          <p className="hidden sm:block">Plan</p>
         </div>
       </Link>
       <Link href="/chat">
-        <div className="flex w-full justify-center py-3">
+        <div className="flex w-full items-center justify-center py-3 sm:gap-2">
           <IoChatbubbleEllipses className="h-6 w-6" />
+          <p className="hidden sm:block">Chat</p>
         </div>
       </Link>
     </div>
