ceaa5fa92b9c4cd8816e1e5432cd06efff7ca0b5
Haikal Hilmi <haikal6255hilmi@gmail.com>
2024-06-01 23:12:06 +0700

    feat(signin): responsive signin

--- src/app/(auth)/signin/page.js
+++ src/app/(auth)/signin/page.js
@@ -30,40 +30,46 @@ export default function SignInHome() {
     }
   }
   return (
-    <div className="flex w-full flex-col items-center justify-center bg-background px-16">
-      <div className="relative aspect-square w-4/5">
-        <Image src="/assets/DestiSmart.png" fill alt="Logo" />
+    <div className="flex min-h-screen w-full flex-row">
+      <div className="relative hidden h-full grow md:block">
+        <div className="absolute z-10 flex h-full w-full bg-black bg-opacity-40" />
+        <Image src="/assets/tugu-home.jpg" className="z-0 object-cover" fill alt="Tugu" />
       </div>
-      <h1 className="mt-4 text-4xl font-medium text-main">DestiSmart</h1>
-      <p className="mt-10 text-text">
-        Fun fact !!
-        <br />
-        Menurut BPS, Indonesia memiliki 2930 usaha objek daya tarik wisata (ODTW) pada 2022
-      </p>
-      <form onSubmit={onFinish} className="mt-20 flex w-full flex-col gap-5">
-        <input
-          required
-          placeholder="email"
-          type="email"
-          className="w-full rounded-full border bg-transparent px-3 py-2"
-        />
-        <input
-          required
-          placeholder="password"
-          type="password"
-          className="w-full rounded-full border bg-transparent px-3 py-2"
-        />
-        {wrong !== '' && <div className="-mb-4 -mt-4 text-sm text-red-600">{wrong}</div>}
-        <button className="rounded-full bg-main px-3 py-2 text-xl text-white" type="submit">
-          Masuk
-        </button>
-        <p className="text-md -mt-3">
-          Belum Punya Akun?{' '}
-          <Link href="/signup">
-            <span className="text-main">Registrasi di sini</span>
-          </Link>
+      <div className="flex h-full w-full grow-0 flex-col items-center justify-center bg-background px-16 md:max-w-md">
+        <div className="relative aspect-square w-1/2 md:w-4/5">
+          <Image src="/assets/DestiSmart.png" fill alt="Logo" />
+        </div>
+        <h1 className="mt-4 text-4xl font-medium text-main">DestiSmart</h1>
+        <p className="mt-10 text-text">
+          Fun fact !!
+          <br />
+          Menurut BPS, Indonesia memiliki 2930 usaha objek daya tarik wisata (ODTW) pada 2022
         </p>
-      </form>
+        <form onSubmit={onFinish} className="mt-20 flex w-full flex-col gap-5">
+          <input
+            required
+            placeholder="email"
+            type="email"
+            className="w-full rounded-full border bg-transparent px-3 py-2"
+          />
+          <input
+            required
+            placeholder="password"
+            type="password"
+            className="w-full rounded-full border bg-transparent px-3 py-2"
+          />
+          {wrong !== '' && <div className="-mb-4 -mt-4 text-sm text-red-600">{wrong}</div>}
+          <button className="rounded-full bg-main px-3 py-2 text-xl text-white" type="submit">
+            Masuk
+          </button>
+          <p className="text-md -mt-3">
+            Belum Punya Akun?{' '}
+            <Link href="/signup">
+              <span className="text-main">Registrasi di sini</span>
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
   )
 }
